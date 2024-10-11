be39aa8ac2aa90b9d61e981d47b219499f092315
fansize <langlang_1991@126.com>
2024-10-11 14:21:51 +0200

    v1.0

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,12 +1,28 @@
-import "~main.css";
+import { useEffect } from "react"
 
+const websites = [
+  "https://www.google.com",
+  "https://www.github.com",
+  "https://www.stackoverflow.com",
+  // 添加更多网站...
+]
 
-function IndexPopup() {
-  return (
-    <div className=''>
-      <h1>Hello World</h1>
-    </div>
-  )
+function getRandomWebsite() {
+  return websites[Math.floor(Math.random() * websites.length)]
 }
 
-export default IndexPopup
+function Popup() {
+  useEffect(() => {
+    const openRandomWebsite = async () => {
+      const randomWebsite = getRandomWebsite()
+      await chrome.tabs.create({ url: randomWebsite })
+      window.close()
+    }
+
+    openRandomWebsite()
+  }, [])
+
+  return <div>正在打开随机网页...</div>
+}
+
+export default Popup
