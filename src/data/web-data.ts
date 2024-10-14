a925bd17ef651b72c733c0011ba9c8a5206c0e05
fansize <langlang_1991@126.com>
2024-10-14 16:29:40 +0200

    v1.7.2

--- src/data/web-data.ts
+++ src/data/web-data.ts
@@ -21,7 +21,7 @@ export const tools: Tool[] = [
 
 export const popularChannels: ChannelItem[] = [
     { name: "生成Listing", icon: "assets/images/icons/unspeakable.png", description: "根据产品名称、描述、关键字信息生成 Listing 五点描述", url: "https://www.chat123.ai/listing" },
-    { name: "SEO搜索关键词推荐", icon: "assets/images/icons/linus-tech-tips.png", description: "自动优化关键词，提高在搜索引擎中的排名和曝光率", url: "https://www.youtube.com/linustechtips" },
-    { name: "买家消息回复", icon: "assets/images/icons/doctor-mike.png", description: "根据买家消息快速生成符合标准的回复模板", url: "https://www.youtube.com/doctormike" },
-    { name: "亚马逊5点描述优化", icon: "assets/images/icons/mrbeast.png", description: "撰写高效精炼的5点描述,高埋词率,突出商品特色", url: "https://www.youtube.com/mrbeast" },
+    { name: "SEO搜索关键词推荐", icon: "assets/images/icons/linus-tech-tips.png", description: "自动优化关键词，提高在搜索引擎中的排名和曝光率", url: "https://www.chat123.ai/seo-keywords" },
+    { name: "买家消息回复", icon: "assets/images/icons/doctor-mike.png", description: "根据买家消息快速生成符合标准的回复模板", url: "https://www.chat123.ai/amazon-reply" },
+    { name: "亚马逊5点描述优化", icon: "assets/images/icons/mrbeast.png", description: "撰写高效精炼的5点描述,高埋词率,突出商品特色", url: "https://www.chat123.ai/amazon-msyh" },
 ]
